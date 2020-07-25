aa3b354c51bba42adaddfe0278eb11233d95946e
eriktal <eriktalla@gmail.com>
2020-07-25 15:12:13 -0500

    my third commint

---
+++ JavaLab3.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class JavaLab3 {
+    //Calculating hydrostatic pressure
+    static void hydraulic_Pressure_Calculator() {
+        int pressure;
+        int depth = 100;
+        int density = 8;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the depth in ft ");
+        depth = sc.nextInt();
+        System.out.println("Enter the density of the fluid ");
+        density = sc.nextInt();
+        if (depth > 0) {
+            if (density > 0) {
+                pressure = (int) (0.052 * density * depth);
+                System.out.println("The Hydraulic Pressure in psi is " + pressure);
+            }
+        } else {
+            System.out.println("You must enter a positive number");
+        }
+
+    }
+// comparing numbers
+    public void student_age_comparison(){
+        int student1,student2,student3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter first student age");
+        student1 = sc.nextInt();
+        System.out.println(" Enter second student age");
+        student2 = sc.nextInt();
+        System.out.println(" Enter third student age");
+        student3 = sc.nextInt();
+        if(student1 > student2 && student2>student3){
+            System.out.println("Student 1 is the oldest in the class");}
+        else if (student2>student1 && student1>student3){
+                System.out.println( "Student 2 the oldest in the class");}
+        else if (student3>student2 && student2>student1){
+            System.out.println("Student 3 is the oldest");
+                }
+
+        }
+
+    public static void main(String[] args) {
+        hydraulic_Pressure_Calculator();
+        JavaLab3 ojb = new JavaLab3();
+        ojb.student_age_comparison();
+    }
+}
+
+
+
+
+
---
+++ LargestNumber.java
@@ -0,0 +1,41 @@
+public class SRT {
+
+    public static void main(String[] args) {
+        int num2 = 20;
+        int num3 = 30;
+        if (num2 == num3) {
+            System.out.println(" num2 and num3 are equal");
+        } else {
+            System.out.println("num2 and num3 are not equal");
+        }
+
+        if (num2 != num3) {
+            System.out.println("num2 is different than num3");
+        } else {
+            System.out.println("num2 is equal to num3"); }
+
+        if (num2 % 2 == 0) {
+            System.out.println("num2 is even");
+        } else {
+            System.out.println("num2 is odd");
+        }
+        if (num2 > num3) {
+            System.out.println("num2 is greater than num3");
+        } else {
+            System.out.println("num2 is not greater than num3");
+        }
+            int x = 10 , z;
+            int y = 100;
+            z = (x== 10)? 200 : 500;
+            System.out.println("Z is equal to " + z); }
+
+
+    }
+
+
+
+
+
+
+
+
