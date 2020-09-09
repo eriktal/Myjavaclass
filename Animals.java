805a05a23c87fb537efca7c2acf1a9136842e076
eriktal <eriktalla@gmail.com>
2020-09-09 10:40:51 -0500

    First commit

--- Animals.java
+++ Animals.java
@@ -1,44 +1,51 @@
 package Com.learning.Week8;
 
-    public class Animals {
-        public Animals(){
-        }
-        public void animalSounds(){
-            System.out.println("The animal makes a sound");
-        }
+public class Animals {
+    public Animals(){
     }
-    class Pigs extends Animals{
-        public Pigs(){
-            super();
-        }
-        public void animalSounds(){
-            System.out.println("Pigs oink");
-        }
+    public void animalSounds(){
+        System.out.println("The animal makes a sound");
     }
-    class Cats extends Animals{
-        public Cats(){
-            super();
-        }
-        public void animalSounds() {
-            System.out.println("cats meow");
-        }
+}
+class Pigs extends Animals{
+    public Pigs(){
+        super();
+    }
+    public void animalSounds(){
+        System.out.println("Pigs oink");
+    }
+}
+class Cats extends Animals{
+    public Cats(){
+        super();
+    }
+    public void animalSounds() {
+        System.out.println("cats meow");
+    }
 
-        public void animalSounds(int num) {
-            for (int count = 1; count <= num; ) {
-                System.out.println("The cat meowed");
-                count++;
-            }
-            System.out.println("The cat has meowed "+ num +" times");
+    public void animalSounds(int num) {
+        for (int count = 1; count <= num; ) {
+            System.out.println("The cat meowed");
+            count++;
         }
+        System.out.println("The cat has meowed "+ num +" times");
+    }
 
+}
+class Test{
+    public static void main(String[] args) {
+        Cats cat = new Cats();
+        Pigs pig = new Pigs();
+        cat.animalSounds();
+        cat.animalSounds(2);
+        pig.animalSounds();
     }
-    class Test{
-        public static void main(String[] args) {
-            Cats cat = new Cats();
-            Pigs pig = new Pigs();
-            cat.animalSounds();
-            cat.animalSounds(2);
-            pig.animalSounds();
-        }
     }
+
+
+
+
+
+
+
 
